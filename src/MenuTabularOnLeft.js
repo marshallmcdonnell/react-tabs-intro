4680d645704ace8a3a9dba55d402ac08ffd94178
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-05-23 17:14:18 -0400

    Added dynamic tab addition methods

--- src/MenuTabularOnLeft.js
+++ src/MenuTabularOnLeft.js
@@ -2,53 +2,82 @@ import React, { Component } from 'react';
 import { Grid, Menu, Segment, Icon } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
+// Initial data
+const tabName = 'dataset2';
 
+const initialDatasets = {
+    "dataset1": {
+        title: "Dataset 1",
+        display: <Segment>I am Dataset 1</Segment>,
+        isActive: false
+    },
+    "dataset2": {   
+        title: "Dataset 2",
+        display: <Segment>I am Dataset 2</Segment>,
+        isActive: false
+    }
+}
 
+// Tab Component
+function MenuTab(props) {
+    return (
+        <Menu.Item key={props.name} active={props.isActive} onClick={props.changeTab} as='a'> 
+            {props.title}  <Icon name='remove circle' onClick={props.removeTab}/> 
+        </Menu.Item>
+    );
+}
+
+// Tabs Component
 export default class MenuTabularOnLeft extends Component {
-    constructor() {
-        super();
-    
-        const initialDatasets = {
-            "dataset1": {
-                title: "Dataset 1",
-                display: <Segment>I am Dataset 1</Segment>
-            },
-            "dataset2": {   
-                title: "Dataset 2",
-                display: <Segment>I am Dataset 2</Segment>
-            }
-        }
-
-        const tabName = 'dataset2';
-
-        this.state = { 
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: tabName,
             datasets: initialDatasets,
-            activeItem: tabName,
-            renderTab: initialDatasets[tabName].display
-        }
-    };
+            display: <h1>Hello, select a tab</h1>
+        };
+    }
 
-    changeTab(tabName) {
-        const renderTab = this.state.datasets[tabName].display
+    changeTab(tabName, display) {
         this.setState({ 
-            renderTab: renderTab,
             activeItem: tabName,
-         })
+            display: display,
+        })
     }
 
-    removeTab = () => {
-        console.log('index');
+    removeTab(tabName) {
+        console.log("Removing: " + tabName);
     }
+
+    renderTabFromDataset(i, dataset) {
+        const activeItem = this.state.activeItem;
+        const isActive = (activeItem === dataset) ? true : false;
+        return <MenuTab
+                key={i}
+                name={dataset}
+                title={dataset.title}
+                display={dataset.display}
+                isActive={isActive}
+                changeTab={() => this.changeTab(dataset, dataset.display)}
+                removeTab={() => this.removeTab(dataset.title)}
+            />
+    }
+
+    renderTabs() {
+        const datasets = this.state.datasets;
+        var tabs = [];
+        tabs.push(this.renderTabFromDataset(0, datasets['dataset1']));
+        tabs.push(this.renderTabFromDataset(1, datasets['dataset2']));
+        return tabs;
+    }
+
 
     render() {
         const activeItem = this.state.activeItem;
-        const renderTab  = this.state.renderTab;      
+        const display  = this.state.display;              
         
-        
-        const myArray = [
-            <Menu.Item key='dataset1' active={activeItem === 'dataset1'} onClick={() => this.changeTab('dataset1')} as='a'> Dataset1  <Icon name='remove circle' onClick={() => this.removeTab()}/> </Menu.Item>,
-            <Menu.Item key='dataset2' active={activeItem === 'dataset2'} onClick={() => this.changeTab('dataset2')} as='a'> Dataset2  <Icon name='remove circle' onClick={() => this.removeTab()}/> </Menu.Item>
-        ]
+        var tabs = this.renderTabs();
+        console.log(tabs);
         
 
         //const myArray = this.createTabs();
@@ -58,12 +87,13 @@ export default class MenuTabularOnLeft extends Component {
                 <Grid>
                     <Grid.Column width={4}>
                         <Menu fluid vertical tabular>
-                            {myArray}
+
+                            {tabs}
                         </Menu>
                     </Grid.Column>
 
                     <Grid.Column stretched width={12}>
-                        {renderTab}
+                        {display}
                     </Grid.Column>
                 </Grid>
             </div>
