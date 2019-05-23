638cb0410bb41243b5afd05e1ea143ceaa487917
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-05-23 15:32:26 -0400

    Added semantics-ui-css as package

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from "react";
 import { Container } from "semantic-ui-react";
 import MenuTabularOnLeft from "./MenuTabularOnLeft";
 
-// TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
-
 
 class App extends React.Component {
     render() {
