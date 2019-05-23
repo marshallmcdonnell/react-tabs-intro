e0cb881c713f11dfb77da4cae4058c73613b440a
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-05-23 15:33:17 -0400

    Semantics-UI tabs w/ disabled button and refactor for displays

--- src/MenuTabularOnLeft.js
+++ src/MenuTabularOnLeft.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import { Grid, Menu, Segment, Icon } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+
 
 export default class MenuTabularOnLeft extends Component {
-    state = { 
-      activeItem: 'dataset1',
-      renderTab: 'dataset1' 
-    }
+    constructor() {
+        super();
+    
+        const initialDatasets = {
+            "dataset1": {
+                title: "Dataset 1",
+                display: <Segment>I am Dataset 1</Segment>
+            },
+            "dataset2": {   
+                title: "Dataset 2",
+                display: <Segment>I am Dataset 2</Segment>
+            }
+        }
+
+        const tabName = 'dataset2';
+
+        this.state = { 
+            datasets: initialDatasets,
+            activeItem: tabName,
+            renderTab: initialDatasets[tabName].display
+        }
+    };
 
     changeTab(tabName) {
+        const renderTab = this.state.datasets[tabName].display
         this.setState({ 
-            renderTab: tabName,
+            renderTab: renderTab,
             activeItem: tabName,
          })
     }
@@ -20,46 +42,31 @@ export default class MenuTabularOnLeft extends Component {
 
     render() {
         const activeItem = this.state.activeItem;
-        const renderTab  = this.state.renderTab;
+        const renderTab  = this.state.renderTab;      
+        
+        
+        const myArray = [
+            <Menu.Item key='dataset1' active={activeItem === 'dataset1'} onClick={() => this.changeTab('dataset1')} as='a'> Dataset1  <Icon name='remove circle' onClick={() => this.removeTab()}/> </Menu.Item>,
+            <Menu.Item key='dataset2' active={activeItem === 'dataset2'} onClick={() => this.changeTab('dataset2')} as='a'> Dataset2  <Icon name='remove circle' onClick={() => this.removeTab()}/> </Menu.Item>
+        ]
+        
 
+        //const myArray = this.createTabs();
         return (
-            <Grid>
-                <Grid.Column width={4}>
-                    <Menu fluid vertical tabular>
-                        <Menu.Item name='dataset1' active={activeItem === 'dataset1'} onClick={() => this.changeTab('dataset1')} as='a'>
-                            Dataset1   <Icon name='remove circle' active={activeItem === 'dataset2'} onClick={() => this.removeTab()}/>
-                        </Menu.Item>
-                        <Menu.Item name='dataset2' active={activeItem === 'dataset2'} onClick={() => this.changeTab('dataset2')} as='a'>
-                            Dataset2  <Icon name='remove circle' onClick={() => this.removeTab()}/>
-                        </Menu.Item>
-                    </Menu>
-                </Grid.Column>
+            <div>
+                <button>Add Dataset 3</button>
+                <Grid>
+                    <Grid.Column width={4}>
+                        <Menu fluid vertical tabular>
+                            {myArray}
+                        </Menu>
+                    </Grid.Column>
 
-                <Grid.Column stretched width={12}>
-                    <RenderedContent tabName={renderTab} />
-                </Grid.Column>
-            </Grid>
+                    <Grid.Column stretched width={12}>
+                        {renderTab}
+                    </Grid.Column>
+                </Grid>
+            </div>
         )
     }
 }
-
-const RenderedContent = ({ tabName }) => {
-    if (tabName === 'dataset1') {
-        return <Dataset1 />
-    }
-    if (tabName === 'dataset2') {
-        return <Dataset2 />
-    }
-}
-
-const Dataset1 = () => (
-    <Segment>
-        This is Dataset 1
-    </Segment>
-)
-
-const Dataset2 = () => (
-    <Segment>
-        This is Dataset 2
-    </Segment>
-)
