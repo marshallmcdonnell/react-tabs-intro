f4143010945d0548cd54f29cfebe16254d68ac4b
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-05-30 12:04:35 -0400

    Added dropdowns for method, tech, type selection

--- app/src/index.js
+++ app/src/index.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
-import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import store from './store';
 
-import { Actions, jsonformsReducer } from '@jsonforms/core';
-import { materialRenderers } from '@jsonforms/material-renderers';
-
-import personSchema from './schemas/personSchema';
-import addressSchema from './schemas/addressSchema';
-
-// Create store
-const store = createStore(
-    combineReducers( { jsonforms: jsonformsReducer() }),
-    { jsonforms: { renderers: materialRenderers }  }
-);
-
-// Combine sub-schema to single for intial store
-const schema = {
-    type: 'object',
-    properties: {
-        person: personSchema,
-        address: addressSchema
-    }
-}
-
-// Initialize store
-store.dispatch(Actions.init({}, schema));
+import App from './App';
 
 ReactDOM.render(
 <Provider store={store}>
