9ff400da6090a23b314baebeeca81b3f7e3a2ccf
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-06-04 10:23:25 -0400

    Refactoring scidata tabs

--- app/src/upload/scidata-tabs/SciDataTab.js
+++ app/src/upload/scidata-tabs/SciDataTab.js
@@ -1,42 +1,38 @@
 import React from "react";
-import { Menu, Icon, Segment } from "semantic-ui-react";
+import PropTypes from "prop-types";
+import { Menu, Icon } from "semantic-ui-react";
 
 import "semantic-ui-css/semantic.min.css";
 
 // Tab Component
-export function SciDataTab(props) {
-  return (
-    <Menu.Item
-      key={props.name}
-      active={props.isActive}
-      onClick={props.changeTab}
-      as="a"
-    >
-      {props.title} <Icon name="remove circle" onClick={props.removeTab} />
-    </Menu.Item>
-  );
+export default class  SciDataTab extends React.Component {
+  static propTypes = {
+    name: PropTypes.string,
+    title: PropTypes.string,
+    isActive: PropTypes.bool,
+    changeTab: PropTypes.func,
+    removeTab: PropTypes.func,
+  };
+
+  render() {
+    const {
+      name,
+      title,
+      isActive,
+      changeTab,
+      removeTab
+    } = this.props;
+
+    return (
+      <Menu.Item
+        key={name}
+        active={isActive}
+        onClick={changeTab}
+        as="a"
+      >
+        {title} <Icon name="remove circle" onClick={removeTab} />
+      </Menu.Item>
+    );
+  }
 }
 
-export function SciDataTabPanel(props) {
-  const display = props.display;
-  const activeItem = props.activeItem;
-
-  return (
-    <div>
-      <Menu pointing secondary>
-        <Menu.Item
-          name="Input"
-          active={activeItem === "Input"}
-          onClick={() => props.changeChildTab("Input")}
-        />
-        <Menu.Item
-          name="JSON-LD"
-          active={activeItem === "JSON-LD"}
-          onClick={() => props.changeChildTab("JSON-LD")}
-        />
-      </Menu>
-
-      <Segment>{display}</Segment>
-    </div>
-  );
-}
